a6cec2ca8354433581b5d9b0187139ddf5a14d09
rovingwind93@gmail.com <rovingwind93@gmail.com>
2020-12-22 20:47:14 +0800

    123

---
+++ JSHomework/javascript/homework1.js
@@ -0,0 +1,32 @@
+﻿function homework1() {
+    var str = "";
+    str += "<table>";
+    for (var x = 1; x <= 9; x++) {
+        str += "<tr>";
+        for (var y = 1; y <= 9; y++) {
+            if (y * x < 10)
+                str += "<td>" + y + "&nbsp;x&nbsp;" + x + "&nbsp;=&nbsp;&nbsp;" + y * x + "</td>";
+            else
+                str += "<td>" + y + "&nbsp;x&nbsp;" + x + "&nbsp;=&nbsp;" + y * x + "</td>";
+        }
+        str += "</tr>";
+    }
+    str += "</table>";
+    str = "<h2>Homework1</h2>" + str;
+
+    var ele = document.getElementById("IDsection");
+    ele.innerHTML = str;    
+}
+
+function homework2() {
+    var str = "<h2>Homework2</h2>";
+    str += "<form action=\"#\" method=\"get\" enctype=\"multipart / form - data\">";
+    str += "<fieldset><legend>表單</legend>";
+    str += "<div><label for=\"account1\" class=\"t1\">姓名</label><input type=\"text\" id=\"account1\" name=\"account\" placeholder=\"王建民\" size=\"10\" autocomplete=\"off\" /></div>";
+    str += "<div><label for=\"pwd1\" class=\"t1\">密碼</label><input type=\"password\" Id=\"pwd1\" name=\"pwd\" placeholder=\"請輸入密碼\" maxlength=\"6\" /></div>";
+    str += "<div><label for=\"date1\" class=\"t1\">日期</label><input type=\"text\" id=\"date1\" name=\"date\" placeholder=\"2020 / 12 / 22\" autocomplete=\"off\" /></div>";
+    str += "<div><input type=\"submit\" value=\"送出\" /></div>";
+    str += "</fieldset></form>";
+    var ele = document.getElementById("IDsection");
+    ele.innerHTML = str;
+}
