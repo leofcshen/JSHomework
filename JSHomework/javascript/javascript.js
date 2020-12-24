d1146218281d5ed1f001d958ad5be0b601e6d165
rovingwind93@gmail.com <rovingwind93@gmail.com>
2020-12-24 14:47:09 +0800

    123

--- JSHomework/javascript/javascript.js
+++ JSHomework/javascript/javascript.js
@@ -291,9 +291,10 @@ function homework4() {
 }
 
 function homework5() {
-    
+    alert("施工中");
     var str = "<h2>Homework5</h2>";
-    str += "<div><input type=\"text\" id=\"idName\" name=\"name\" placeholder=\"\" size=\"10\" autocomplete=\"off\" />年</div>";
+    //str += "<div><input type=\"text\" id=\"idName\" name=\"name\" placeholder=\"\" size=\"10\" autocomplete=\"off\" />年</div>";
+    str += "<img src=\"/images/work.gif\">";
     var ele = document.getElementById("IDsection");
     ele.innerHTML = str;
 }
