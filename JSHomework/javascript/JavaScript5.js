240fc3d53e8d4e5663f68589eb8999439e04f391
Leo Shen <rovingwind93@gmail.com>
2021-01-02 13:21:24 +0800

    123

--- JSHomework/javascript/JavaScript5.js
+++ JSHomework/javascript/JavaScript5.js
@@ -1,75 +1,81 @@
-﻿function homework5() {
-    //alert("施工中");
+﻿function homework5() {    
     var str = "<h2>Homework5</h2>";
-    str += "<div class= 'h5_div'>";    
-    str += "<label>年</label><select id ='idYear' name ='year'>";    
-    for (let i = 2010; i < 2021; i++) {        
-        str += `<option value=${i}>${i}</option>`;        
+    str += "<div class= 'h5_div'>"; 
+    str += "<label>年</label><select id='idYear' name='year'>";
+    for (let i = 2010; i < 2021; i++) {
+        str += `<option>${i}</option>`;
     }
-    str += "</select > ";
-    str += "<label>月</label><select  id ='idMonth' name ='month'>";    
+    str += "</select>";
+    str += "<label>月</label><select id='idMonth' name='month'>";
     for (let i = 1; i < 13; i++) {
-        str += `<option value=${i}>${i}</option>`;
+        str += `<option>${i}</option>`;
     }
-    str += "</select > ";
-    str += "<label>日</label><select id ='idDay' name ='day'>";
+    str += "</select>";
+    str += "<label>日</label><select id='idDay' name='day'>";
     for (let i = 1; i < 32; i++) {
-        str += `<option value=${i}>${i}</option>`;
-    }    
-    str += "</select > ";
+        str += `<option>${i}</option>`;
+    }
+    str += "</select>";
     str += "<h2>請選擇日期</h2>";
     str += "<h2 id='idShow'>日期為：2010/1/1</h2>";
     str += "</div>";
+    
     var ele = document.getElementById("IDsection");
     ele.innerHTML = str;
-    //binding 年月日改變時功能
-    document.getElementById('idYear').addEventListener("change", changeYear);
-    document.getElementById('idMonth').addEventListener("change",changeMonth);
-    document.getElementById('idDay').addEventListener("change", changeDay);
+    //binding 年月日欄位改變時的 function
+    document.getElementById("idYear").addEventListener("change", changeYear);
+    document.getElementById("idMonth").addEventListener("change", changeMonth);
+    document.getElementById("idDay").addEventListener("change", changeDay);
 
-    var lastDay, dayNumberOld=31, dayNumberNew, year=2010, month=1, day=1;
-
-    function changeYear() {//年改變
-        year = document.getElementById('idYear').value;//取得年欄位
-        lastDay = new Date(year, month, 0);//取得該年該月最後日期
-        dayNumberNew = lastDay.getDate();//取得當月天數
-        dayNumberChange();
-        changeDay();
+    var year = 2010, month = 1, day = 1;
+    var lastDate;//存放當月最後一天的完整日期格式
+    var dayNumberNew;//存放新的月總天數
+    var dayNumberOld = 31;//存放舊的月總天數
+    //年欄位改變時
+    function changeYear() {
+        year = document.getElementById("idYear").value;//取得年欄位
+        lastDate = new Date(year, month, 0);//取得該年該月最後一天的完整日期格式
+        dayNumberNew = lastDate.getDate(); //取得當月天數        
+        dayNumberChange();//新增刪減日的總天數
+        show();//show 出所選定的日期
     }
-
+    //月欄位改變時
     function changeMonth() {
-        month = document.getElementById('idMonth').value;//取得月欄位
-        lastDay = new Date(year, month, 0);//取得該年該月最後日期
-        dayNumberNew = lastDay.getDate();//取得當月天數
-        dayNumberChange();
-        changeDay();
+        month = document.getElementById("idMonth").value;//取得月欄位
+        lastDate = new Date(year, month, 0);//取得該年該月最後日期
+        dayNumberNew = lastDate.getDate(); //取得當月天數        
+        dayNumberChange();//新增刪減日的總天數
+        show();//show 出所選定的日期
     }
+    //日欄位改變時
     function changeDay() {
-        day = document.getElementById('idDay').value;        
-        document.getElementById('idShow').innerHTML = `日期為：${year}/${month}/${day}`;
+        day = document.getElementById("idDay").value;//取得日欄位
+        show();
     }
-
-    function deleteDay() {//刪除多餘日期
-        //document.getElementById('idDay').options.length=0;
-        console.log("delete");
-        for (let i = 1; i <= dayNumberOld - dayNumberNew; i++) {
-            document.getElementById('idDay').options.remove(dayNumberNew);            
-        }      
-    }
-    function addDay() {//新增缺少的日期        
-        console.log("add");
-        for (let i = dayNumberOld; i < dayNumberNew; i++) {
-            console.log(i);
-            document.getElementById('idDay').options.add(new Option(i+1));
-        }
-    }
-
-    function dayNumberChange() {
-        console.log(dayNumberNew + " " + dayNumberOld);
+    //show 出所選定的日期
+    function show() {
+        document.getElementById("idShow").innerHTML = `日期為：${year}/${month}/${day}`;
+    }    
+    //當月的總天數改變時，判斷日欄位要新增還是刪減
+    function dayNumberChange() {        
         if (dayNumberNew > dayNumberOld)
             addDay();
         else if (dayNumberNew < dayNumberOld)
-            deleteDay();
-        dayNumberOld = dayNumberNew;
+            deleteDay();        
+        dayNumberOld = dayNumberNew;//把舊的總天數用新的總天數取代掉用做下一次判斷
+    }    
+    //新增 select 的日期
+    function addDay() {
+        //for (let i = 1; i <= dayNumberNew; i++) {
+        //    document.getElementById("idDay").options.add(new Option(i));
+        //}
+        for (let i = dayNumberOld; i < dayNumberNew; i++)//舊的比新的多幾天就加幾次
+            document.getElementById("idDay").options.add(new Option(i + 1));
+    }
+    //刪減 select 的日期
+    function deleteDay() {
+        //document.getElementById("idDay").options.length = 0;//刪除全部 select option
+        for (let i = 1; i <= dayNumberOld - dayNumberNew; i++)//新的比舊的多幾天就刪幾次
+            document.getElementById("idDay").options.remove(dayNumberNew);
     }
 }
---
+++ JSHomework/javascript/JavaScriptBubble.js
@@ -0,0 +1,23 @@
+﻿function homework6() {
+    var str = "<h2>HomeworkBubble</h2>";
+    str += "<div class='h6_div'>";    
+    str += "<img id='idimg1' src='images/star.gif' />";
+    str += "<div class='h6_div'>";
+    str += "<img id='idimg2' src='images/star.gif' />";
+    str += "<div class='h6_div'>";
+    str += "<img id='idimg3' src='images/star.gif' />";
+    str += "<div class='h6_div'>";
+    str += "<img id='idimg4' src='images/star.gif' />";
+    str += "<div class='h6_div'>";
+    str += "<img id='idimg5' src='images/star.gif' />";
+    str += "</div>";
+    str += "</div>";
+    str += "</div>";
+    str += "</div>";
+    str += "<h2 id='idH2'>尚未評點</h2>";
+    str += "<input id='idBtnReset'type ='button' value='重新評點' />";
+    str += "</div>";
+
+    var ele = document.getElementById("IDsection");
+    ele.innerHTML = str;
+}
